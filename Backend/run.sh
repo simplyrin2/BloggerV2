751692ef45bcad3afb3eb1cd4b6041d20f534fea
Indranil Patra <70438225+simplyrin2@users.noreply.github.com>
2023-06-18 16:37:56 +0530

    Update run.sh

--- Backend/run.sh
+++ Backend/run.sh
@@ -4,8 +4,8 @@ export DATABASE_URI=sqlite:///database.sqlite3
 export SECRET_KEY=iamasecretkey1234!@#$
 export UPLOADS=$PWD/static/images/uploads
 export JWT_SECRET_KEY=thisisasecretkey
-export SENDER_EMAIL_ADDRESS="inpat2020@gmail.com"
-export SENDER_EMAIL_PASSWORD="hdxzbrbboqlobcxk"
-export WEBHOOK_URL="https://chat.googleapis.com/v1/spaces/AAAAFdiV4EQ/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=4BbVL_uTtg8byQ-OQniOx5muCRNLxQl0uo-b2WD_VeQ%3D"
+export SENDER_EMAIL_ADDRESS="example@gmail.com"
+export SENDER_EMAIL_PASSWORD="thisisapassword"
+export WEBHOOK_URL="https://thisisawebhookurl"
 
 python api.py
